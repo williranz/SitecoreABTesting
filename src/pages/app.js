2305c726a58ffbcdf2d862ecc867925371c41566
DK\wcw <wcw@sitecore.net>
2020-10-27 18:42:03 +0800

    fix error

--- src/pages/app.js
+++ src/pages/app.js
@@ -4,14 +4,13 @@ import Home from '../components/home';
 import About from '../components/about';
 import Contact from '../components/contact';
 
-const App = () => (
-  <Layout>
+const App = () => {
+  return (
     <Router basepath="/app">
       <Home path="/home" />
       <About path="/about" />
       <Contact path="/contact" />
-      <Default path="/" />
     </Router>
-  </Layout>
-)
+  )
+}
 export default App
