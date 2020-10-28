bf0a3b77332b2fcb31c97f9302835bab85801f7e
DK\wcw <wcw@sitecore.net>
2020-10-28 11:05:38 +0800

    set working routing

--- src/pages/app.js
+++ src/pages/app.js
@@ -6,10 +6,13 @@ const App = () => (
     <h1>Tutorial!</h1>
     <nav>
       <Link to="app">Home</Link>{" "}
-      <Link to="dashboard">Dashboard</Link>
+      <Link to="app/dashboard">Dashboard</Link> {" "}
+      <Link to="app/invoices/123">Invoice 123</Link>{" "}
+      <Link to="app/invoices/abc">Invoice ABC</Link>
       <Router>
-        <Home path="/app" message="Hi! Welcome"/>
-        <Dashboard path="/dashboard" message="This is your dashboard"/>
+        <Home path="app" message="Hi! Welcome"/>
+        <Dashboard path="app/dashboard" message="This is your dashboard"/>
+        <Invoice path="app/invoices/:invoiceId" />
       </Router>
     </nav>
   </div>
@@ -24,6 +27,12 @@ const Home = ({message}) => (
 const Dashboard = ({message}) => (
   <div>
     <h2>{message}</h2>
+  </div>
+);
+
+const Invoice = (props) => (
+  <div>
+    <h2>Invoice {props.invoiceId}</h2>
   </div>
 );
 
